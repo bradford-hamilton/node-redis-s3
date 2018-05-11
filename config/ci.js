07ba6b0c9bbc4ff1103fec9135c16130cee9ce51
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2018-05-11 09:53:22 -0600

    Setup travis ci

--- config/ci.js
+++ config/ci.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
   googleClientID: `${process.env.GOOGLE_CLIENT_ID}`,
   googleClientSecret: `${process.env.GOOGLE_CLIENT_SECRET}`,
-  mongoURI: `mongodb://${process.env.MONGO_USERNAME_DEV}:${process.env.MONGO_PASSWORD_DEV}@ds117250.mlab.com:17250/adv_nod_experiment`,
+  mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
   cookieKey: `${process.env.COOKIE_KEY}`,
   redisUrl: 'redis://127.0.0.1:6379'
 };
